0ceed8960b908ad09a904f18ab263727d4e03a86
badderXkaido <badre.mouhoub@gmail.com>
2018-07-28 16:49:17 +0100

    test message first commit

---
+++ src/javaTest/JavaTest.java
@@ -0,0 +1,18 @@
+package javaTest;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		String key="One";
+		
+		switch (key) {
+		case "One":
+			System.out.println("The value Is One");
+			break;
+
+		default:
+			System.out.println("The value Isn't One");
+			break;
+		}
+	}
+}
